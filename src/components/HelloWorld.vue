d0fd1db64cd07e73d879996443513ad1aafb85cd
imndx <imndxx@gmail.com>
2024-08-26 19:51:02 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -146,7 +146,7 @@ initProductRequirementList()
                     <div v-for="(req, index) in checkedProductRequirementList" :key="index">
                         <p v-if="req.id !== 'linux'" class="desc">{{ req.name }}</p>
                         <div v-else-if="req.id === 'linux'">
-                            <p v-for="(arch) in req.archList.filter(a => a.checked)" :key="arch.name">{{ `Linux PC-${arch.name}` }}</p>
+                            <p v-for="(arch) in req.archList.filter(a => a.checked)" :key="arch.name">{{ `Linux 客户端(${arch.name})` }}</p>
                         </div>
                     </div>
                 </div>
