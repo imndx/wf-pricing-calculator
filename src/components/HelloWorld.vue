d9014013be8cbd04eb3d802aa622a103e35a6551
imndx <imndxx@gmail.com>
2024-08-26 19:37:00 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -52,9 +52,19 @@ const computedCurReq = computed(() => {
 const checkedProductRequirementList = computed(() => productRequirementList.value.filter(req => req.checked === true))
 
 
-const computedCommerialProductList = computed(() => {
+const computedCommercialProductList = computed(() => {
     let map = new Map()
     for (const pr of checkedProductRequirementList.value) {
+        if (pr.id === 'linux') {
+            for (const arch of pr.archList) {
+                if (arch.checked) {
+                    for (const id of arch.dependencies) {
+                        let cp = commercial_products.filter(p => p.id === id)[0]
+                        map.set(id, cp)
+                    }
+                }
+            }
+        }
         if (pr.dependencies.length > 0) {
             for (const id of pr.dependencies) {
                 let cp = commercial_products.filter(p => p.id === id)[0]
@@ -63,6 +73,25 @@ const computedCommerialProductList = computed(() => {
         }
     }
     return [...map.values()];
+})
+
+const nextButtonEnable = computed(() => {
+    let curReq = computedCurReq.value
+    console.log('xxxxxxxx')
+    if (curReq.id === 'linux') {
+        if (!curReq.checked) {
+            return true;
+        } else {
+            for (const arch of curReq.arch) {
+                if (arch.checked) {
+                    return true
+                }
+            }
+            return false;
+        }
+    } else {
+        return true;
+    }
 })
 
 initProductRequirementList()
@@ -85,6 +114,16 @@ initProductRequirementList()
                     <input :checked="computedCurReq.checked" type="radio" @change="computedCurReq.checked = true"/>
                     是
                 </label>
+
+                <div v-if="computedCurReq.id === 'linux' && computedCurReq.checked" class="arch-list">
+                    <p>请选择需要支持的 CPU 架构</p>
+                    <div v-for="(arch) in computedCurReq.archList" :key="arch.name">
+                        <label>
+                            <input :checked="arch.checked" type="checkbox" @change="console.log('xxx', $event);arch.checked = $event.target.checked"/>
+                            {{ arch.name }}
+                        </label>
+                    </div>
+                </div>
                 <label>
                     <input :checked="!computedCurReq.checked" type="radio" @change="computedCurReq.checked = false"/>
                     否
@@ -95,17 +134,20 @@ initProductRequirementList()
                 <button @click="initProductRequirementList">
                     重置
                 </button>
-                <button @click="curReqIndex ++">
+                <button @click="curReqIndex ++" :disabled="computedCurReq.id === 'linux' && computedCurReq.checked && computedCurReq.archList.filter(a => a.checked).length === 0">
                     下一项
                 </button>
             </div>
         </div>
         <div v-else-if="curReqIndex === computedProductRequirementList.length" style="height: 100%">
-            <div v-if="checkedProductRequirementList.filter(req => req.id !== 'linux').length > 0" class="checked_req_item">
+            <div v-if="checkedProductRequirementList.length > 0" class="checked_req_item">
                 <div style="flex: 1">
                     <p class="title">你的产品需求如下，请确认</p>
-                    <div v-for="(req, index) in checkedProductRequirementList.filter(req => req.id !== 'linux')" :key="index">
-                        <p class="desc">{{ req.name }}</p>
+                    <div v-for="(req, index) in checkedProductRequirementList" :key="index">
+                        <p v-if="req.id !== 'linux'" class="desc">{{ req.name }}</p>
+                        <div v-else-if="req.id === 'linux'">
+                            <p v-for="(arch) in req.archList.filter(a => a.checked)" :key="arch.name">{{ `Linux PC-${arch.name}` }}</p>
+                        </div>
                     </div>
                 </div>
                 <div class="action-container">
@@ -130,11 +172,11 @@ initProductRequirementList()
         <div v-else class="report">
             <div style="flex: 1">
                 <p class="title">你的产品需求对应的费用详情如下，请参考</p>
-                <p>{{ '总价：' + computedCommerialProductList.map(p => p.price).reduce((pre, cur, index) => pre + cur, 0) }}</p>
                 <p>费用详情：</p>
-                <div v-for="(cp, index) in computedCommerialProductList" :key="index">
+                <div v-for="(cp, index) in computedCommercialProductList" :key="index">
                     <p>{{ cp.name + ' ' + cp.price }}</p>
                 </div>
+                <p>{{ '合计：' + computedCommercialProductList.map(p => p.price).reduce((pre, cur, index) => pre + cur, 0) }}</p>
             </div>
             <div class="action-container">
                 <button @click="initProductRequirementList">
@@ -209,9 +251,12 @@ button:active {
     border-radius: 4px;
 }
 
-
 .req_item form label input {
     margin-right: 10px;
+}
+
+.req_item .arch-list {
+    margin-left: 30px;
 }
 
 .checked_req_item {
